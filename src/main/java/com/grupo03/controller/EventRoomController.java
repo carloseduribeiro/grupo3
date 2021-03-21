c8525a559595b69d7408dceee8cd37dab59f5a22
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-03-20 21:59:11 -0300

    Método getEventCapacity adicionado; Removido comentários desnecessários;

--- src/main/java/com/grupo03/controller/EventRoomController.java
+++ src/main/java/com/grupo03/controller/EventRoomController.java
@@ -3,6 +3,10 @@ package com.grupo03.controller;
 import com.grupo03.model.EventRoom;
 import com.grupo03.model.dao.EventRoomDao;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 
 /**
  * Realiza as operações da entidade EventRoom aplicando as regras de negócio.
@@ -17,15 +21,12 @@ public class EventRoomController {
     /**
      * Instancia do Dao para realizar as operações no banco.
      */
-    private final EventRoomDao eventRoomDao;
+    private static final EventRoomDao eventRoomDao = new EventRoomDao();
 
     /**
-     * Construtor sem argumento.<br>
-     * Instancia um objeto da classe EventRoomDao.
+     * Construtor sem argumento.
      */
-    public EventRoomController() {
-        this.eventRoomDao = new EventRoomDao();
-    }
+    public EventRoomController() {}
 
     /**
      * Cadastra um novo espaço de evento no banco de dados.
@@ -36,26 +37,40 @@ public class EventRoomController {
      */
     public boolean create(String name, int capacity) {
 
-        var result = true; // Armazena o resultado da operação.
+        var result = true;      // Armazena o resultado da operação.
 
-        /* Verifica se o nome tem pelo menos 3 caracteres ou se não passa de 255
-         * que é o limite do banco: */
         if (name.length() < 3 || name.length() > 255) {
             result = false;
-        }
-
-        /* Verifica se a capacidade do espaço de pelo menos 2 lugares:
-         * 1 aluno + 1 professor por exemplo.*/
-        if (capacity < 2) {
+        } else if (capacity < 2) {
             result = false;
         }
 
-        // Verifica se nenhum erro foi encontrado:
         if (result) {
             var eventRoom = new EventRoom(name, capacity);
-            this.eventRoomDao.save(eventRoom);
+            eventRoomDao.save(eventRoom);
         }
 
         return result;
+    }
+
+    /**
+     * Consulta e retorna a capacidade máxima de pessoas que podem ser
+     * cadastradas no evento.
+     * @return  a capacidade máxima do evento.
+     */
+    public static int getEventCapacity() {
+
+        int lessCapacity;
+        int amountRooms;
+
+        List<EventRoom> eventRoomList = eventRoomDao.getAll();
+        amountRooms = eventRoomList.size();
+
+        Optional<EventRoom> lessCapacityRoom =
+                            eventRoomList.stream().min(Comparator.comparing(EventRoom::getCapacity));
+        lessCapacity = lessCapacityRoom.map(EventRoom::getCapacity).orElse(0);
+
+         /* Menor capacidade * quantidade de salas = capacidade máxima do evento: */
+        return amountRooms * lessCapacity;
     }
 }
